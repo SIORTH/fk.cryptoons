21142de4bf9e698c1c3d591feae9acf9406369e1
mufax18 <32135831+mufax18@users.noreply.github.com>
2017-11-21 01:28:29 +0700

    Update PageSafeLink.js

--- PageSafeLink.js
+++ PageSafeLink.js
@@ -14,11 +14,11 @@ function auto_safeconvert(){var a_to_vd=window.location.hostname;if(protected_li
 {protected_links=a_to_vd;}
 var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElementsByTagName("a");a_to_va=a_to_ve.length;a_to_vf=a_to_fa();a_to_vg=a_to_vf.length;var a_to_vh=false;var j=0;var a_to_vi="";for(var i=0;i<a_to_va;i++)
 {a_to_vh=false;j=0;while(a_to_vh==false&&j<a_to_vg)
-{a_to_vi=a_to_ve[i].href;if(a_to_vi.match(a_to_vf[j])||!a_to_vi||!a_to_vi.match("https"))
+{a_to_vi=a_to_ve[i].href;if(a_to_vi.match(a_to_vf[j])||!a_to_vi||!a_to_vi.match("http"))
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="harianinhil.blogspot.co.id/p/fkcryptoons.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="http://harianinhil.blogspot.com/p/YOUR-PAGE.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
