408439dd3ccfde93f4eeb5d1c8a7713e0c0305d7
mufax18 <32135831+mufax18@users.noreply.github.com>
2017-11-20 23:44:31 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -131,7 +131,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "https://harianinhil.blogspot.com/p/x-adv-si-skutik-adventure.html?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://harianinhil.blogspot.com/p/fkcryptoons.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
