6db5777a4fbbb3c64f43d8ef501965aa080898fd
mufax18 <32135831+mufax18@users.noreply.github.com>
2017-11-21 02:19:46 +0700

    Create protectlink.js

---
+++ protectlink.js
@@ -0,0 +1,3 @@
+//<![CDATA[  
+protected_links = "fajars.kazuya.us"; (function() { var sl = document.createElement('script'); sl.type = 'text/javascript'; sl.async = true; sl.src = 'https://cdn.rawgit.com/mufax18/fk.cryptoons/544bd05e/PageSafeLink.js'; (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(sl); })(); 
+//]]> 
